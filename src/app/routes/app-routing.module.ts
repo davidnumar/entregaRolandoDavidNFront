83618e2c6432dfef0fbba4ca992bca970f8baa60
Núñez Martínez <dnunezm@minsait.com>
2022-12-15 16:50:24 +0100

    first

---
+++ src/app/routes/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { componentFactoryName } from '@angular/compiler';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ClienteDetailComponent } from '../cliente/cliente-detail/cliente-detail.component';
+import { ClienteFormComponent } from '../cliente/cliente-form/cliente-form.component';
+import { ClienteMainComponent } from '../cliente/cliente-main/cliente-main.component';
+import { LoginComponent } from '../usuario/login/login.component';
+
+const routes: Routes = [
+
+  {
+    path:'',
+    component:ClienteMainComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'cliente/new',
+    component:ClienteFormComponent
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'cliente/edit/:id',
+    component:ClienteFormComponent
+  },
+  {
+    path:'cliente/view/:id',
+    component: ClienteDetailComponent
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
